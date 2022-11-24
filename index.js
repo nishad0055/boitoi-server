69456d38aede9d495fe161026c3028e7715dced2
nishadahmed5 <nishatahmed650@gmail.com>
2022-11-24 19:56:17 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const app = express()
+
+const port = process.env.PORT || 5000
+
+//middleware
+app.use(express.json())
+app.use(cors())
+
+
+app.get('/', (req, res)=>{
+    res.send('book resale server')
+})
+
+app.listen(port, ()=>{
+    console.log(`Book resale server is running on port ${port}`)
+})
